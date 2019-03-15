980c7528cd3030cc0c1a7da95eb47a479430939f
Arsan Irianto <arsan.irianto@gmail.com>
2019-03-15 15:53:48 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,71 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+let map, infoWindow;
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+  componentDidMount() {
+    loadScript(
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyBNeksjt_QfncrS2CAgbajesDOvw02ulOs&callback=initMap"
     );
+    window.initMap = this.initMap;
   }
+
+  initMap = () => {
+    map = new window.google.maps.Map(document.getElementById("map"), {
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 6
+    });
+
+    infoWindow = new window.google.maps.InfoWindow();
+
+    const navigator = window.navigator;
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent(
+            `You are here <br> latitude: ${pos.lat} <br> longitude: ${pos.lng}`
+          );
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          this.handleLocationError(false, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      this.handleLocationError(false, infoWindow, map.getCenter());
+    }
+  };
+
+  handleLocationError = (browserHasGeolocation, infoWindow, pos) => {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+      browserHasGeolocation
+        ? "Error: The Geolocation service failed."
+        : "Error: Your browser doesn't support geolocation."
+    );
+    infoWindow.open(map);
+  };
+
+  render() {
+    return <div id="map" />;
+  }
+}
+
+function loadScript(url) {
+  var index = window.document.getElementsByTagName("script")[0];
+  var script = window.document.createElement("script");
+  script.src = url;
+  script.async = true;
+  script.defer = true;
+  index.parentNode.insertBefore(script, index);
 }
 
 export default App;
